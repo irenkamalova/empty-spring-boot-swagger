3d77b8fe2045814a30e752bf40f952db7d02f988
irenkamalova <iren.kamalova@gmail.com>
2018-06-26 13:58:49 +0300

    Init commit

--- src/main/java/hello/SwaggerConfig.java
+++ src/main/java/hello/SwaggerConfig.java
@@ -46,12 +46,8 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Spring Boot REST API")
-                .description("\"Spring Boot REST API for Online Store\"")
+                .title("Spring Boot + Swagger Example")
                 .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .contact(new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
                 .build();
     }
 }
