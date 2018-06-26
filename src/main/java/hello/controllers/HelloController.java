08c2f8f5e18d7e07e97ebe6abeaa1aff8dffc11b
irenkamalova <iren.kamalova@gmail.com>
2018-06-26 14:08:55 +0300

    Init commit

--- src/main/java/hello/controllers/HelloController.java
+++ src/main/java/hello/controllers/HelloController.java
@@ -1,16 +1,15 @@
 package hello.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
-
+    
     @RequestMapping("/")
-    @ResponseBody
     public String index() {
         return "Greetings from Spring Boot!";
     }
+  
 
 }
